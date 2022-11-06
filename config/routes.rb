40efd0cbf69184cbd0241a0f75356bd36f26cc08
mkannapally <akannapa@chicagobooth.edu>
2022-11-06 01:06:46 +0000

    users url almost works!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get("/users" , {:controller => "users" , :action => "index"})
+
+  get("/users/:username" ,{:controller => "users" , :action => "details"} )
+  
 end
